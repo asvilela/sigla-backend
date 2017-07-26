5e10f57150f13a1c9cca5b7ca0408c0f49328e1e
André Vilela <andre_vilella@hotmail.com>
2017-07-26 22:17:51 +0000

    Created user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { build(:user) }
+
+  it 'validate_presence_of email' do
+    expect(user).to validate_presence_of(:email)  
+  end
+  it 'validate_uniqueness_of email' do
+    expect(user).to validate_uniqueness_of(:email).case_insensitive  
+  end
+  it 'validate_confirmation_of password' do
+    expect(user).to validate_confirmation_of(:password)  
+  end
+  it 'allow_value email' do
+    expect(user).to allow_value('teste@teste.com.br').for(:email)
+  end
 end
