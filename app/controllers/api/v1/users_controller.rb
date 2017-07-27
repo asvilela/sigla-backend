62b892994f2be6b7443a34f133ac9cadcdc7bbdc
André Vilela <andre_vilella@hotmail.com>
2017-07-27 04:24:23 +0000

    Adding user controller

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,44 @@
+class Api::V1::UsersController < ApplicationController
+
+  def show
+    begin
+      user = User.find(params[:id])
+      render json: user, status: :ok
+    rescue
+      head :not_found
+    end
+  end
+
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      render json: user, status: :created
+    else
+      render json: { errors: user.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    user = User.find(params[:id])
+
+    if user.update(user_params)
+      render json: user, status: :ok
+    else
+      render json: { errors: user.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    head :no_content
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+
+end
